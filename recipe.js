af222dd0fd34545639a575f2f2417810711f3f05
Lenard Frommelt <lenny09918050@googlemail.com>
2019-11-10 03:11:08 +0000

    reduced to most generic recipe without individualization

--- recipe.js
+++ recipe.js
@@ -4,52 +4,9 @@ module.exports = {
         ["merge", "createEmpty"],
         {
             //baseRemote"
-            merge: ["root-template", "*"]
+            merge: ["*"]
         }
     ],
 
-    output: [
-        ["submodule"],
-        ["create"],
-        {
-            //thingyModuleType"
-            create: [null]
-        }
-    ],
-
-    sources: [
-        ["submodule"],
-        ["create", "merge", "use"],
-        {
-            //thingyModuleType"
-            create: [null],
-            //baseRemote"
-            merge: ["*"],
-            //baseRemote"
-            use: ["*"]
-        }
-    ],
-
-    testing: [
-        ["directory"],
-        ["create"],
-        {
-            create: [null]
-        }
-    ],
-
-    toolset: [
-        ["submodule"],
-        ["use"],
-        {
-            //baseRemote"
-            use: ["toolset"]
-        }
-    ],
-
-    "*": true,
-
-    individualize() {
-        return "individual"
-    }
+    "*": true
 }
